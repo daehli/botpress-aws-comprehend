48928b0738a4871005a22c64180b10375aa4c437
daehli <daehlinadeauotis@gmail.com>
2017-12-05 19:32:19 -0500

    Promisify AWS comprehend

--- src/index.js
+++ src/index.js
@@ -7,33 +7,13 @@
 const fs = require('fs')
 const path = require('path')
 const _ = require('lodash')
-const _AWS = require('aws-sdk')
 const Promise = require('bluebird')
+const _AWS = require('aws-sdk')
 
 // TODO polyfill require => const configTemplate = require(raw!.botpress-aws-comprehend.config.yml)
 import configTemplate from 'raw!./botpress-aws-comprehend.config.yml'
 
 let comprehend = null
-
-const incomingMiddleWare = (event, next) => {
-  // PUT CONFIG
-
-  const message = _.get(event, 'text')
-
-  if (message) {
-    comprehend.detectEntities({ Text: message, LanguageCode: 'en' }, (err, data) => {
-      if (!err) {
-        event.aws_comprehend = data
-        console.log(data)
-        next()
-      }
-    })
-  }
-}
-
-const initAWSComprehend = (bp, configurator) => {
-  comprehend = new _AWS.Comprehend({ ...configurator })
-}
 
 const createConfigFile = bp => {
   const name = 'botpress-aws-comprehend.config.yml'
@@ -49,12 +29,33 @@ const createConfigFile = bp => {
   }
 }
 
+const incomingMiddleWare = async (event, next) => {
+  const message = _.get(event, 'text')
+  if (message) {
+    comprehend
+      .detectEntitiesAsync({ Text: message, LanguageCode: 'en' })
+      .then(data => {
+        event.aws_comprehend = data
+        next()
+      })
+      .catch(err => {
+        event.bp.logger.debug('botpress-aws-comprehend error: ', err)
+        next()
+      })
+  }
+}
+
+const initAWSComprehend = (bp, configurator) => {
+  comprehend = Promise.promisifyAll(new _AWS.Comprehend({ ...configurator }))
+}
+
 module.exports = {
   config: {
     accessKeyId: { type: 'string', required: true, default: '', env: 'AWD_ACCESS_KEY_ID' },
     secretAccessKey: { type: 'string', required: true, default: '', env: 'AWS_SECRET_ACCESS_KEY' },
     region: { type: 'string', required: true, default: '', env: 'AWS_REGION' },
     apiVersion: { type: 'string', required: true, default: '', env: 'AWS_API_VERSION' }
+    // TODO add more Config
   },
 
   init: async function(bp, configurator) {
@@ -73,8 +74,6 @@ module.exports = {
     initAWSComprehend(bp, config)
 
     createConfigFile(bp)
-    // This is called before ready.
-    // At this point your module is just being initialized, it is not loaded yet.
   },
 
   ready: async function(bp, configurator) {
